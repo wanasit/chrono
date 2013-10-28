eaf675615620db3b0d674b75fda4f31b9e33c7b5
Wanasit Tanakitrungruang <wanasit@Thyme.local>
2013-10-28 20:03:20 +0900

    Update karma configuration with QUnit

--- karma.conf.js
+++ karma.conf.js
@@ -1,74 +1,75 @@
 // Karma configuration
-// Generated on Sat Jun 22 2013 14:46:29 GMT+0900 (JST)
+// Generated on Mon Oct 28 2013 19:53:46 GMT+0900 (JST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+    // frameworks to use
+    frameworks: ['qunit'],
+    
+    // list of files / patterns to load in the browser
+    files: [
+      'moment.js',
+      'chrono.min.js',
+      'chrono.js',
+      'timezone.js',
+      'parsers/*.js',
+      'parsers/*/*.js',
+      'refiners/*.js',
+      'refiners/*/*.js',
+      'test-qunit/Test*.js',
+    ],
 
 
-// list of files / patterns to load in the browser
-files = [
-  QUNIT,
-  QUNIT_ADAPTER,
-  'moment.js',
-  'chrono.min.js',
-  'chrono.js',
-  'timezone.js',
-  'parsers/*.js',
-  'parsers/*/*.js',
-  'refiners/*.js',
-  'refiners/*/*.js',
-  'test-qunit/Test*.js',
-];
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
 
-// list of files to exclude
-exclude = [
-  
-];
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['progress'],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['progress'];
+    // web server port
+    port: 9876,
 
 
-// web server port
-port = 9876;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// cli runner port
-runnerPort = 9100;
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['Chrome'],
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = true;
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
-
-
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
-
-
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
