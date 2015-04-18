0a38c6653240dcdcaa116e7febe2d2b4f11917a5
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2015-04-18 17:01:23 +0900

    Add postfix pattern to weekday

--- src/parsers/EN/ENWeekdayParser.js
+++ src/parsers/EN/ENWeekdayParser.js
@@ -6,10 +6,21 @@ var moment = require('moment');
 var Parser = require('../parser').Parser;
 var ParsedResult = require('../../result').ParsedResult;
 
-var PATTERN = /(\W|^)((\,|\(|\（)\s*)?((this|last|next)\s*)?(Sunday|Sun|Monday|Mon|Tuesday|Tues|Tue|Wednesday|Wed|Thursday|Thurs|Thur|Friday|Fri|Saturday|Sat)(\s*(\,|\)|\）))?(\W|$)/i;
+var PATTERN = new RegExp('(\\W|^)' + 
+    '(?:(?:\\,|\\(|\\（)\\s*)?' + 
+    '(?:(this|last|next)\\s*)?' + 
+    '(Sunday|Sun|Monday|Mon|Tuesday|Tues|Tue|Wednesday|Wed|Thursday|Thurs|Thur|Friday|Fri|Saturday|Sat)' +
+    '(?:\\s*(?:\\,|\\)|\\）))?' + 
+    '(?:\\s*(this|last|next)\\s*week)?' + 
+    '(?=\\W|$)', 'i');
+
 var DAYS_OFFSET = { 'sunday': 0, 'sun': 0, 'monday': 1, 'mon': 1,'tuesday': 2, 'tues':2, 'tue':2, 'wednesday': 3, 'wed': 3,
     'thursday': 4, 'thurs':4, 'thur': 4, 'thu': 4,'friday': 5, 'fri': 5,'saturday': 6, 'sat': 6,}
-    
+
+var PREFIX_GROUP = 2;
+var WEEKDAY_GROUP = 3;
+var POSTFIX_GROUP = 4;
+
 exports.Parser = function ENWeekdayParser() {
     Parser.call(this);
 
@@ -18,30 +29,32 @@ exports.Parser = function ENWeekdayParser() {
     this.extract = function(text, ref, match, opt){ 
         
         var index = match.index + match[1].length;
-        var text = match[0].substr(match[1].length, match[0].length - match[9].length - match[1].length);
+        var text = match[0].substr(match[1].length, match[0].length - match[1].length);
         var result = new ParsedResult({
             index: index,
             text: text,
             ref: ref,
         });
-
-        var dayOfWeek = match[6].toLowerCase();
+        
+        var dayOfWeek = match[WEEKDAY_GROUP].toLowerCase();
         var offset = DAYS_OFFSET[dayOfWeek];
         if(offset === undefined) return null;
         
         var startMoment = moment(ref);
-        var prefix = match[5];
-        if (prefix) {
-            prefix = prefix.toLowerCase();
+        var prefix = match[PREFIX_GROUP];
+        var postfix = match[POSTFIX_GROUP];
+
+        if (prefix || postfix) {
+            var norm = prefix || postfix;
+            norm = norm.toLowerCase();
             
-            if(prefix == 'last')
+            if(norm == 'last')
                 startMoment.day(offset - 7)
-            else if(prefix == 'next')
+            else if(norm == 'next')
                 startMoment.day(offset + 7)
-            else if(prefix== 'this')
+            else if(norm== 'this')
                 startMoment.day(offset);
-        }
-        else{
+        } else{
             var refOffset = startMoment.day();
 
             if (Math.abs(offset - 7 - refOffset) < Math.abs(offset - refOffset)) {
--- test-qunit/test_en_weekday.js
+++ test-qunit/test_en_weekday.js
@@ -87,6 +87,48 @@ test("Test - Single Expression", function() {
         var expectDate = new Date(2012, 7, 3, 12);
         ok(Math.abs(expectDate.getTime() - resultDate.getTime()) < 100000, 'Test result.startDate ' + resultDate +'/' +expectDate)
     }
+
+
+    var text = "Let's have a meeting on Friday next week";
+    var results = chrono.casual.parse(text, new Date(2015, 3, 18));
+    ok(results.length == 1, JSON.stringify( results ) )
+
+    var result = results[0];
+    if(result){
+        ok(result.index == 24, 'Wrong index')
+        ok(result.text == 'Friday next week', result.text )
+
+        ok(result.start, JSON.stringify(result.start) )
+        ok(result.start.get('year') == 2015, 'Test Result - (Year) ' + JSON.stringify(result.start) )
+        ok(result.start.get('month') == 4, 'Test Result - (Month) ' + JSON.stringify(result.start) )
+        ok(result.start.get('day') == 24, 'Test Result - (Day) ' + JSON.stringify(result.start) )
+        ok(result.start.get('weekday') == 5, 'Test Result - (Weekday) ' + JSON.stringify(result.start) )
+
+        var resultDate = result.start.date();
+        var expectDate = new Date(2015, 3, 24, 12);
+        ok(Math.abs(expectDate.getTime() - resultDate.getTime()) < 100000, 'Test result.startDate ' + resultDate +'/' +expectDate)
+    }
+
+
+    var text = "I plan on taking the day off on Tuesday, next week";
+    var results = chrono.casual.parse(text, new Date(2015, 3, 18));
+    ok(results.length == 1, JSON.stringify( results ) )
+
+    var result = results[0];
+    if(result){
+        ok(result.index == 32, 'Wrong index')
+        ok(result.text == 'Tuesday, next week', result.text )
+
+        ok(result.start, JSON.stringify(result.start) )
+        ok(result.start.get('year') == 2015, 'Test Result - (Year) ' + JSON.stringify(result.start) )
+        ok(result.start.get('month') == 4, 'Test Result - (Month) ' + JSON.stringify(result.start) )
+        ok(result.start.get('day') == 21, 'Test Result - (Day) ' + JSON.stringify(result.start) )
+        ok(result.start.get('weekday') == 2, 'Test Result - (Weekday) ' + JSON.stringify(result.start) )
+
+        var resultDate = result.start.date();
+        var expectDate = new Date(2015, 3, 21, 12);
+        ok(Math.abs(expectDate.getTime() - resultDate.getTime()) < 100000, 'Test result.startDate ' + resultDate +'/' +expectDate)
+    }
 });
 
 
