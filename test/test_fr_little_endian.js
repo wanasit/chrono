bba309a26c37fb38e561d66396089eaab303d045
benoit <benoit@localhost.localdomain>
2016-11-13 13:39:02 +0100

    added fr first day of month parsing

--- test/test_fr_little_endian.js
+++ test/test_fr_little_endian.js
@@ -22,6 +22,26 @@ test("Test - Single expression", function() {
     }
 
 
+    var text = "1er Août 2012";
+    var results = chrono.parse(text, new Date(2012,7,1));
+    ok(results.length == 1, JSON.stringify( results ) );
+
+    var result = results[0];
+    if (result) {
+        ok(result.start, JSON.stringify(result.start) );
+        ok(result.start.get('year') == 2012, 'Test Result - (Year) ' + JSON.stringify(result.start) );
+        ok(result.start.get('month') == 8, 'Test Result - (Month) ' + JSON.stringify(result.start) );
+        ok(result.start.get('day') == 1, 'Test Result - (Day) ' + JSON.stringify(result.start) );
+
+        ok(result.index == 0, 'Wrong index');
+        ok(result.text == '1er Août 2012', result.text );
+
+        var resultDate = result.start.date();
+        var expectDate = new Date(2012, 8-1, 1, 12);
+        ok(Math.abs(expectDate.getTime() - resultDate.getTime()) < 100000, 'Test result.startDate ' + resultDate +'/' +expectDate)
+    }
+
+
     var text = "10 Août 234 AC";
     var results = chrono.parse(text, new Date(2012,7,10));
     ok(results.length == 1, JSON.stringify( results ) )
