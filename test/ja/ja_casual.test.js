4938c870a72215d79106884f0836ac4e34426919
Wanasit Tanakitrungruang <berrysboy@gmail.com>
2020-04-19 14:43:14 +0900

    refactor: ja test cases

--- test/ja/ja_casual.test.js
+++ test/ja/ja_casual.test.js
@@ -1,48 +1,37 @@
-var chrono = require('../../src/chrono');
+import * as chrono from '../../src/chrono';
+import { testSingleCase } from '../test_util';
 
 
 test("Test - Single Expression", function() {
 
-    var text = "今日感じたことを忘れずに";
-    var results = chrono.parse(text, new Date(2012, 8-1, 10, 12));
-    expect(results.length).toBe(1)
+    testSingleCase(chrono, '今日感じたことを忘れずに', new Date(2012, 8-1, 10, 12), (result) => {
+        expect(result.index).toBe(0);
+        expect(result.text).toBe('今日');
 
-    var result = results[0];
-    if (result) {
-        expect(result.index).toBe(0)
-        expect(result.text).toBe('今日')
+        expect(result.start).not.toBeNull();
+        expect(result.start.get('year')).toBe(2012);
+        expect(result.start.get('month')).toBe(8);
+        expect(result.start.get('day')).toBe(10);
 
-        expect(result.start).not.toBeNull()
-        expect(result.start.get('year')).toBe(2012)
-        expect(result.start.get('month')).toBe(8)
-        expect(result.start.get('day')).toBe(10)
-        
         var resultDate = result.start.date();
         var expectDate = new Date(2012, 8-1, 10, 12);
         expect(expectDate.getTime()).toBeCloseTo(resultDate.getTime())
-    }
+    });
 
 
-    var text = "昨日の全国観測値ランキング";
-    var results = chrono.parse(text, new Date(2012,8-1, 10, 12));
-    expect(results.length).toBe(1)
+    testSingleCase(chrono, '昨日の全国観測値ランキング', new Date(2012,8-1, 10, 12), (result) => {
+        expect(result.index).toBe(0);
+        expect(result.text).toBe('昨日');
 
-    var result = results[0];
-    if (result) {
-        expect(result.index).toBe(0)
-        expect(result.text).toBe('昨日')
+        expect(result.start).not.toBeNull();
+        expect(result.start.get('year')).toBe(2012);
+        expect(result.start.get('month')).toBe(8);
+        expect(result.start.get('day')).toBe(9);
 
-        expect(result.start).not.toBeNull()
-        expect(result.start.get('year')).toBe(2012)
-        expect(result.start.get('month')).toBe(8)
-        expect(result.start.get('day')).toBe(9)
-        
         var resultDate = result.start.date();
         var expectDate = new Date(2012, 8-1, 9, 12);
         expect(expectDate.getTime()).toBeCloseTo(resultDate.getTime())
-    }
-
-    
+    });
 });
 
 
