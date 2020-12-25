0abddad2e49ddf55d74bf08dc32cc946fb9f03ad
Mitchell <contact@mitchells.work>
2020-12-25 19:57:44 +0100

    feat(parsers(en/nl)) allow for useShorts

--- src/locales/nl/parsers/NLTimeUnitWithinFormatParser.ts
+++ src/locales/nl/parsers/NLTimeUnitWithinFormatParser.ts
@@ -1,15 +1,21 @@
-import { TIME_UNITS_PATTERN, parseTimeUnits } from "../../en/constants";
+import { TIME_UNITS_PATTERN, parseTimeUnits, FULL_TIME_UNITS_PATTERN } from "../../en/constants";
 import { ParsingContext } from "../../../chrono";
 import { ParsingComponents } from "../../../results";
 import { AbstractParserWithWordBoundaryChecking } from "../../../common/parsers/AbstractParserWithWordBoundary";
 
+const PATTERN = createRegExp(TIME_UNITS_PATTERN);
+const FULL_PATTERN = createRegExp(FULL_TIME_UNITS_PATTERN);
+
+function createRegExp(pattern: string): RegExp {
+    return new RegExp(`(?:binnen|in)\\s*` + "(" + pattern + ")" + `(?=\\W|$)`, "i");
+}
 export default class ENTimeUnitWithinFormatParser extends AbstractParserWithWordBoundaryChecking {
-    innerPattern(): RegExp {
-        return new RegExp(`(?:binnen|in)\\s*` + "(" + TIME_UNITS_PATTERN + ")" + `(?=\\W|$)`, "i");
+    innerPattern(context: ParsingContext): RegExp {
+        return context.option.useShorts ? FULL_PATTERN : PATTERN;
     }
 
     innerExtract(context: ParsingContext, match: RegExpMatchArray): ParsingComponents {
-        const timeUnits = parseTimeUnits(match[1]);
+        const timeUnits = parseTimeUnits(match[1], context.option.useShorts);
         return ParsingComponents.createRelativeFromRefDate(context.refDate, timeUnits);
     }
 }
