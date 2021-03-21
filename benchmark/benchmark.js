ac08a8ca138fa26875102a1b1f0ac111585549af
Wanasit Tanakitrungruang <wanasit.wt@gmail.com>
2021-03-21 13:52:04 +0900

    New: Adding basic benchmark

---
+++ benchmark/benchmark.js
@@ -0,0 +1,18 @@
+const chrono = require("..");
+const b = require("benny");
+
+b.suite(
+    "Example",
+
+    b.add("Parsing empty string", () => {
+        chrono.parse("");
+    }),
+
+    b.add("Parsing slash date - 11/12/2021", () => {
+        chrono.en.parseDate("11/12/2021");
+    }),
+
+    b.cycle(),
+    b.complete(),
+    b.save({ file: "chrono", format: "chart.html" })
+);
