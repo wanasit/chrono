4c4a4136c6ac4ccfa02e0b5e11dea48416727a13
Georg Mayr-Duffner <georg.mayr-duffner@wu.ac.at>
2022-03-10 23:19:54 +0100

    [DE] Improve time units ago and later

--- src/locales/de/parsers/DETimeUnitRelativeFormatParser.ts
+++ src/locales/de/parsers/DETimeUnitRelativeFormatParser.ts
@@ -12,28 +12,40 @@ export default class DETimeUnitRelativeFormatParser extends AbstractParserWithWo
 
     innerPattern(): RegExp {
         return new RegExp(
-            `(?:\\s*((?:nächste|kommende|folgende|letzte|vergangene|vorige|vor(?:her|an)gegangene)(?:s|n|m|r)?|vor|in)\\s*)?` +
+            `(?:((?:(?:über)(?:über)nächste|kommende|folgende|(?:vor)?(?:vor)?letzte|vergangene|vorige|vor(?:her|an)gegangene)(?:s|n|m|r)?|vor|in|nach|ab\\sjetzt(?:\\sin)?)\\s*)?` +
                 `(${NUMBER_PATTERN})?` +
                 `(?:\\s*(nächste|kommende|folgende|letzte|vergangene|vorige|vor(?:her|an)gegangene)(?:s|n|m|r)?)?` +
-                `\\s*(${matchAnyPattern(TIME_UNIT_DICTIONARY)})`,
+                `\\s*(${matchAnyPattern(TIME_UNIT_DICTIONARY)}\\b)` +
+                `(?:\\s*(früher|frueher|vorher|davor|zuvor|später|spaeter|danach|nachher|ab\\sjetzt))?`,
             "i"
         );
     }
 
     innerExtract(context: ParsingContext, match: RegExpMatchArray) {
-        const num = match[2] ? parseNumberPattern(match[2]) : 1;
-        const unit = TIME_UNIT_DICTIONARY[match[4].toLowerCase()];
-        let timeUnits = {};
-        timeUnits[unit] = num;
-
         // Modifier
-        let modifier = match[1] || match[3] || "";
+        let modifier = match[1] || match[3] || match[5] || "";
         modifier = modifier.toLowerCase();
         if (!modifier) {
             return;
         }
 
-        if (/vor/.test(modifier) || /letzte/.test(modifier) || /vergangen/.test(modifier)) {
+        let num = match[2] ? parseNumberPattern(match[2]) : 1;
+        if (/vorvorletzte/.test(modifier) || /überübernächste/.test(modifier)) {
+            num += 2;
+        } else if (/vorletzte/.test(modifier) || /übernächste/.test(modifier)) {
+            num += 1;
+        }
+
+        const unit = TIME_UNIT_DICTIONARY[match[4].toLowerCase()];
+        let timeUnits = {};
+        timeUnits[unit] = num;
+
+        if (
+            /vor/.test(modifier) ||
+            /letzte/.test(modifier) ||
+            /vergangen/.test(modifier) ||
+            /fr(?:ü|ue)her/.test(modifier)
+        ) {
             timeUnits = reverseTimeUnits(timeUnits);
         }
 
