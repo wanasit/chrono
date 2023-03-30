a8db2e4a53d069f2bbd37a5c6735c1deb7378050
yuichkun <yuichkun@gmail.com>
2023-03-30 20:35:32 +0900

    [JA] add support for Hiragana in CasualDateParser

--- test/ja/ja_casual.test.ts
+++ test/ja/ja_casual.test.ts
@@ -2,27 +2,34 @@ import * as chrono from "../../src/";
 import { testSingleCase } from "../test_util";
 
 test("Test - Single Expression", function () {
-    testSingleCase(chrono.ja, "今日感じたことを忘れずに", new Date(2012, 8 - 1, 10, 12), (result) => {
-        expect(result.index).toBe(0);
-        expect(result.text).toBe("今日");
+    function testToday(pattern: string) {
+        testSingleCase(chrono.ja, `${pattern}感じたことを忘れずに`, new Date(2012, 8 - 1, 10, 12), (result) => {
+            expect(result.index).toBe(0);
+            expect(result.text).toBe(pattern);
+            expect(result.start).not.toBeNull();
+            expect(result.start.get("year")).toBe(2012);
+            expect(result.start.get("month")).toBe(8);
+            expect(result.start.get("day")).toBe(10);
+            expect(result.start).toBeDate(new Date(2012, 8 - 1, 10, 12));
+        });
+    }
 
-        expect(result.start).not.toBeNull();
-        expect(result.start.get("year")).toBe(2012);
-        expect(result.start.get("month")).toBe(8);
-        expect(result.start.get("day")).toBe(10);
+    testToday("今日");
+    testToday("きょう");
 
-        expect(result.start).toBeDate(new Date(2012, 8 - 1, 10, 12));
-    });
+    function testYesterday(pattern: string) {
+        testSingleCase(chrono.ja, `${pattern}の全国観測値ランキング`, new Date(2012, 8 - 1, 10, 12), (result) => {
+            expect(result.index).toBe(0);
+            expect(result.text).toBe(pattern);
 
-    testSingleCase(chrono.ja, "昨日の全国観測値ランキング", new Date(2012, 8 - 1, 10, 12), (result) => {
-        expect(result.index).toBe(0);
-        expect(result.text).toBe("昨日");
+            expect(result.start).not.toBeNull();
+            expect(result.start.get("year")).toBe(2012);
+            expect(result.start.get("month")).toBe(8);
+            expect(result.start.get("day")).toBe(9);
 
-        expect(result.start).not.toBeNull();
-        expect(result.start.get("year")).toBe(2012);
-        expect(result.start.get("month")).toBe(8);
-        expect(result.start.get("day")).toBe(9);
-
-        expect(result.start).toBeDate(new Date(2012, 8 - 1, 9, 12));
-    });
+            expect(result.start).toBeDate(new Date(2012, 8 - 1, 9, 12));
+        });
+    }
+    testYesterday("昨日");
+    testYesterday("きのう");
 });
