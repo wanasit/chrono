3ee9da55685722d8b69bd1faa7d7a61232b22be2
yuichkun <yuichkun@gmail.com>
2023-03-30 20:45:22 +0900

    [JA] add more tests for casual dates

--- test/ja/ja_casual.test.ts
+++ test/ja/ja_casual.test.ts
@@ -32,4 +32,60 @@ test("Test - Single Expression", function () {
     }
     testYesterday("昨日");
     testYesterday("きのう");
+
+    function testTomorrow(pattern: string) {
+        testSingleCase(chrono.ja, `${pattern}の天気は晴れです`, new Date(2012, 8 - 1, 10, 12), (result) => {
+            expect(result.index).toBe(0);
+            expect(result.text).toBe(pattern);
+
+            expect(result.start).not.toBeNull();
+            expect(result.start.get("year")).toBe(2012);
+            expect(result.start.get("month")).toBe(8);
+            expect(result.start.get("day")).toBe(11);
+
+            expect(result.start).toBeDate(new Date(2012, 8 - 1, 11, 12));
+        });
+    }
+
+    testTomorrow("明日");
+    testTomorrow("あした");
+
+    function testTonight(pattern: string) {
+        testSingleCase(chrono.ja, `${pattern}には雨が降るでしょう`, new Date(2012, 8 - 1, 10, 12), (result) => {
+            expect(result.index).toBe(0);
+            expect(result.text).toBe(pattern);
+
+            expect(result.start).not.toBeNull();
+            expect(result.start.get("year")).toBe(2012);
+            expect(result.start.get("month")).toBe(8);
+            expect(result.start.get("day")).toBe(10);
+            expect(result.start.get("meridiem")).toBe(chrono.Meridiem.PM);
+
+            expect(result.start).toBeDate(new Date(2012, 8 - 1, 10, 22));
+        });
+    }
+
+    testTonight("今夜");
+    testTonight("こんや");
+    testTonight("今夕");
+    testTonight("こんゆう");
+    testTonight("今晩");
+    testTonight("こんばん");
+
+    function testMorning(pattern: string) {
+        testSingleCase(chrono.ja, `${pattern}食べたパンは美味しかった`, new Date(2012, 8 - 1, 10, 12), (result) => {
+            expect(result.index).toBe(0);
+            expect(result.text).toBe(pattern);
+
+            expect(result.start).not.toBeNull();
+            expect(result.start.get("year")).toBe(2012);
+            expect(result.start.get("month")).toBe(8);
+            expect(result.start.get("day")).toBe(10);
+            expect(result.start.get("meridiem")).toBe(chrono.Meridiem.AM);
+
+            expect(result.start).toBeDate(new Date(2012, 8 - 1, 10, 6));
+        });
+    }
+    testMorning("今朝");
+    testMorning("けさ");
 });
