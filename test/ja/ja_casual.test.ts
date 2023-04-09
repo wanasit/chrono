7da7629aca325e3ab01bb001cb1e727571b009f2
yuichkun <yuichkun@gmail.com>
2023-04-09 17:00:19 +0900

    [JA] test: remove redundant assertions

--- test/ja/ja_casual.test.ts
+++ test/ja/ja_casual.test.ts
@@ -4,12 +4,7 @@ import { testSingleCase } from "../test_util";
 test("Test - Single Expression", function () {
     function testToday(pattern: string) {
         testSingleCase(chrono.ja, `${pattern}感じたことを忘れずに`, new Date(2012, 8 - 1, 10, 12), (result) => {
-            expect(result.index).toBe(0);
             expect(result.text).toBe(pattern);
-            expect(result.start).not.toBeNull();
-            expect(result.start.get("year")).toBe(2012);
-            expect(result.start.get("month")).toBe(8);
-            expect(result.start.get("day")).toBe(10);
             expect(result.start).toBeDate(new Date(2012, 8 - 1, 10, 12));
         });
     }
@@ -19,14 +14,7 @@ test("Test - Single Expression", function () {
 
     function testYesterday(pattern: string) {
         testSingleCase(chrono.ja, `${pattern}の全国観測値ランキング`, new Date(2012, 8 - 1, 10, 12), (result) => {
-            expect(result.index).toBe(0);
             expect(result.text).toBe(pattern);
-
-            expect(result.start).not.toBeNull();
-            expect(result.start.get("year")).toBe(2012);
-            expect(result.start.get("month")).toBe(8);
-            expect(result.start.get("day")).toBe(9);
-
             expect(result.start).toBeDate(new Date(2012, 8 - 1, 9, 12));
         });
     }
@@ -35,14 +23,7 @@ test("Test - Single Expression", function () {
 
     function testTomorrow(pattern: string) {
         testSingleCase(chrono.ja, `${pattern}の天気は晴れです`, new Date(2012, 8 - 1, 10, 12), (result) => {
-            expect(result.index).toBe(0);
             expect(result.text).toBe(pattern);
-
-            expect(result.start).not.toBeNull();
-            expect(result.start.get("year")).toBe(2012);
-            expect(result.start.get("month")).toBe(8);
-            expect(result.start.get("day")).toBe(11);
-
             expect(result.start).toBeDate(new Date(2012, 8 - 1, 11, 12));
         });
     }
@@ -52,15 +33,7 @@ test("Test - Single Expression", function () {
 
     function testTonight(pattern: string) {
         testSingleCase(chrono.ja, `${pattern}には雨が降るでしょう`, new Date(2012, 8 - 1, 10, 12), (result) => {
-            expect(result.index).toBe(0);
             expect(result.text).toBe(pattern);
-
-            expect(result.start).not.toBeNull();
-            expect(result.start.get("year")).toBe(2012);
-            expect(result.start.get("month")).toBe(8);
-            expect(result.start.get("day")).toBe(10);
-            expect(result.start.get("meridiem")).toBe(chrono.Meridiem.PM);
-
             expect(result.start).toBeDate(new Date(2012, 8 - 1, 10, 22));
         });
     }
@@ -74,15 +47,7 @@ test("Test - Single Expression", function () {
 
     function testMorning(pattern: string) {
         testSingleCase(chrono.ja, `${pattern}食べたパンは美味しかった`, new Date(2012, 8 - 1, 10, 12), (result) => {
-            expect(result.index).toBe(0);
             expect(result.text).toBe(pattern);
-
-            expect(result.start).not.toBeNull();
-            expect(result.start.get("year")).toBe(2012);
-            expect(result.start.get("month")).toBe(8);
-            expect(result.start.get("day")).toBe(10);
-            expect(result.start.get("meridiem")).toBe(chrono.Meridiem.AM);
-
             expect(result.start).toBeDate(new Date(2012, 8 - 1, 10, 6));
         });
     }
