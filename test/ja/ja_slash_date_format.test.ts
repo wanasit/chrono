39d50b45eb8802a51c982c1fed186e3db939d692
koji520 <kojima.kazushi06@gmail.com>
2025-05-06 10:05:40 +0900

    test: Add test for range expression in ja_slash_date_format

--- test/ja/ja_slash_date_format.test.ts
+++ test/ja/ja_slash_date_format.test.ts
@@ -37,3 +37,24 @@ test("Test - Single Expression", function () {
         expect(result.start).toBeDate(new Date(2012, 8 - 1, 5, 12));
     });
 });
+
+test("Test - Range Expression", function () {
+    testSingleCase(chrono.ja, "2013/12/26~2014/1/7", new Date(2012, 8 - 1, 10), (result) => {
+        expect(result.index).toBe(0);
+        expect(result.text).toBe("2013/12/26~2014/1/7");
+
+        expect(result.start).not.toBeNull();
+        expect(result.start.get("year")).toBe(2013);
+        expect(result.start.get("month")).toBe(12);
+        expect(result.start.get("day")).toBe(26);
+
+        expect(result.start).toBeDate(new Date(2013, 12 - 1, 26, 12));
+
+        expect(result.end).not.toBeNull();
+        expect(result.end.get("year")).toBe(2014);
+        expect(result.end.get("month")).toBe(1);
+        expect(result.end.get("day")).toBe(7);
+
+        expect(result.end).toBeDate(new Date(2014, 1 - 1, 7, 12));
+    });
+});
