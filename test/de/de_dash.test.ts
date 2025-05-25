2325da79a6a3147471e09a44f08d8f3e192777ba
Wanasit Tanakitrungruang <wanasit.wt@gmail.com>
2025-05-25 09:47:20 +0900

    Refactor: Fix the DE test naming

--- test/de/de_dash.test.ts
+++ test/de/de_dash.test.ts
@@ -1,16 +1,32 @@
 import * as chrono from "../../src/";
 import { testSingleCase } from "../test_util";
 
-test("Test - Should handle de dayname dd-mm-yy", () => {
-    testSingleCase(chrono.de, "30.12.16", (result) => {
+test("Test - Single date expression with dash notation", () => {
+    testSingleCase(chrono.de, "30-12-16", (result) => {
+        expect(result.start.get("year")).toBe(2016);
+        expect(result.start.get("month")).toBe(12);
+        expect(result.start.get("day")).toBe(30);
+    });
+
+    testSingleCase(chrono.de, "Freitag 30-12-16", (result) => {
+        expect(result.text).toBe("Freitag 30-12-16");
         expect(result.start.get("year")).toBe(2016);
         expect(result.start.get("month")).toBe(12);
         expect(result.start.get("day")).toBe(30);
     });
 });
 
-test("Test - Should handle de weekday dd-mm-yy", () => {
+test("Test - Single date expression with dot notation", () => {
+    testSingleCase(chrono.de, "30.12.16", (result) => {
+        expect(result.start.get("year")).toBe(2016);
+        expect(result.start.get("month")).toBe(12);
+        expect(result.start.get("day")).toBe(30);
+    });
+
     testSingleCase(chrono.de, "Freitag 30.12.16", (result) => {
         expect(result.text).toBe("Freitag 30.12.16");
+        expect(result.start.get("year")).toBe(2016);
+        expect(result.start.get("month")).toBe(12);
+        expect(result.start.get("day")).toBe(30);
     });
 });
